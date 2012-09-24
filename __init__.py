c234f2c5354168bb6720a2a093b8fbae21aba5b5
Jason Madden <jason.madden@nextthought.com>
2012-09-24 19:09:45 +0000

    One more potential password validation.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/dataserver@12392 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- __init__.py
+++ __init__.py
@@ -7,3 +7,6 @@
 from nti.monkey import traversing_patch_on_import
 traversing_patch_on_import.patch()
 del traversing_patch_on_import
+
+import zope.i18nmessageid
+MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
