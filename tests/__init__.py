dafd5eece6a078172084acb0f2ec8976c8e892ce
Jason Madden <jason.madden@nextthought.com>
2013-04-08 09:25:22 +0000

    remove all unused imports in tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/dataserver@17988 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,31 +1,8 @@
 """ Tests for the dataserver. """
 
-from unittest import defaultTestLoader
-from unittest import TextTestRunner
-import os
 
 # for export
-from nti.tests import has_attr
-from nti.tests import implements
-from nti.tests import Implements
-from nti.tests import provides
-from nti.tests import Provides
-
-def runner(path, pattern="*.py"):
-	suite = defaultTestLoader.discover(path, pattern)
-	try:
-		runner = TextTestRunner(verbosity=3)
-		for test in suite:
-			runner.run(test)
-	finally:
-		pass
-
-def main():
-	dirname = os.path.dirname( __file__ )
-	if not dirname:
-		dirname = '.'
-	runner( dirname )
-
-if __name__ == '__main__':
-	main()
-
+import nti.tests
+has_attr = nti.tests.has_attr
+provides = nti.tests.provides
+implements = nti.tests.implements
