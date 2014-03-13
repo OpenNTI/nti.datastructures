360366a797cbd78ef6c95d7e1c9ae6a7bd30027e
Josh Zuech <josh.zuech@nextthought.com>
2014-03-13 22:55:19 +0000

    Update utils to use new DATASERVER_DIR env variable
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/dataserver@34841 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@
 # Note that we're not exporting anything by importing it.
 # This helps reduce the chances of import cycles
 
-
 from nti.monkey import traversing_patch_on_import
 traversing_patch_on_import.patch()
 del traversing_patch_on_import
