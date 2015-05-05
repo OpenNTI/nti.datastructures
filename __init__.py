eacf1ee33005f2aa8cfc37fc0359bf668342b782
Jason Madden <jason.madden@nextthought.com>
2015-05-05 13:19:59 +0000

    nti.dataserver was turned into a namespace package for some reason, but the proper declarations were not added here. depending on the order of operations, this can really confuse the import machinery (hitting that on pypy).
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/dataserver@64550 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- __init__.py
+++ __init__.py
@@ -7,9 +7,11 @@
 ## Note that we're not exporting anything by importing it.
 ## This helps reduce the chances of import cycles
 
+# XXXX: Because we are now a namespace package, we have
+# no guarantee this file will ever actually be imported!
 from nti.traversal import monkey as traversing_patch_on_import
 traversing_patch_on_import.patch()
 del traversing_patch_on_import
 
-import zope.i18nmessageid
-MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
+
+__import__('pkg_resources').declare_namespace(__name__)
