cf92af6143aad67ffc8682687b9661905f3dc6f6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-18 14:24:56 -0500

    remove unused imports; pylint

--- src/nti/datastructures/tests/__init__.py
+++ src/nti/datastructures/tests/__init__.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import
 
 # pylint: disable=protected-access,too-many-public-methods,arguments-differ
 
-from nti.testing.layers import find_test
 from nti.testing.layers import GCLayerMixin
 from nti.testing.layers import ZopeComponentLayer
 from nti.testing.layers import ConfiguringLayerMixin
